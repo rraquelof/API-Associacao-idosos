7e32c9797754bae74690c21222392acc18d50bd4
Ana Beatriz <anabeatrizqueiroza@gmail.com>
2025-08-17 20:53:26 -0300

    Adicionando a parte de participação dos idosos nos eventos.

--- src/controllers/eventoController.ts
+++ src/controllers/eventoController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import Evento from "../models/Evento";
 import { ZodError } from "zod";
 import { createEventoSchema } from "../validations/eventoValidations";
+import idoso from "../models/idoso";
 
 export const createEvento = async (req: Request, res: Response) => {
     try{ 
@@ -17,5 +18,31 @@ export const createEvento = async (req: Request, res: Response) => {
       return res.status(400).json({ message: "Dados inválidos", errors: error.issues });
     }
     res.status(500).json({ message: "Erro no servidor ao criar evento" });
+  }
+}
+
+export const addIdososEmEvento = async (req: Request, res: Response) => {
+  try{
+    if (!req.user || req.user.tipo !== "coordenador"){
+      return res.status(401).json({ message: "Somente um coordenador pode adicionar um idoso ao evento" });
+    } 
+
+    const { id } = req.params;
+    const { idosos } = req.body; //arrays de id dos idosos
+
+    const evento = await Evento.findByIdAndUpdate(
+      id,
+      { $addToSet: {idosos: { $each: idosos} } }, //evitar duplicidade
+      { new: true}
+    ).populate("idosos");
+    
+    if(!evento){
+      res.status(500).json({ message: "Erro ao achar o evento" });
+    }
+    res.status(200).json({ message: "Idoso adicionado com sucesso", evento});
+
+  }catch (error: any) {
+    console.error(error);
+    res.status(500).json({ message: "Erro ao adicionar idosos", error: error.message });
   }
 }
--- src/routes/eventoRoutes.ts
+++ src/routes/eventoRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createEvento } from "../controllers/eventoController";
+import { addIdososEmEvento, createEvento } from "../controllers/eventoController";
 import { authenticate } from "../middlewares/autenticacaoMiddleware";
 
 const routerEvento = Router();
 
 routerEvento.post('/cadastroEvento', authenticate, createEvento);
+routerEvento.post('/eventos/:id/idosos', authenticate, addIdososEmEvento);
 
 export default routerEvento;
