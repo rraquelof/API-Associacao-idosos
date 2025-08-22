d3be4857cbb955167187e7b2c51f478871127675
Ana Beatriz <anabeatrizqueiroza@gmail.com>
2025-08-22 15:20:38 -0300

    Adicionando o restante das funções do controller, getById,put,delete.

--- src/controllers/saudeController.ts
+++ src/controllers/saudeController.ts
@@ -3,6 +3,7 @@ import idoso from "../models/idoso";
 import Saude from "../models/Saude";
 import { createSaudeSchema } from "../validations/saudeValidations";
 import Usuario from "../models/Usuario";
+import { id } from "zod/v4/locales/index.cjs";
 
 export const createSaude = async (req: Request, res: Response) => {
     const { usuarioId, idosoId } = (req.body);
@@ -32,12 +33,50 @@ export const getSaude = async (req: Request, res: Response) => {
     const { idosoId } = req.params; // pegar idoso específico 
 
     // Busca todos os registros do idoso
-    const saude = await Saude.find({ idoso: idosoId }).sort({ dataConsulta: -1 });//retorna os registros de todos os idosos
+    const consulta = await Saude.find({ idoso: idosoId }).sort({ dataConsulta: -1 });//retorna os registros de todos os idosos
 
-    if (!saude) {
+    if (!consulta) {
       return res.status(404).json({ message: "Nenhum registro de consulta encontrado" });
     }
 
-    return res.status(200).json(saude);
+    return res.status(200).json(consulta);
+}
+export const getSaudeById = async (req: Request, res: Response) => {
+  const consultas = await Saude.find({ idosoId: req.params.idosoId }).sort({ dataConsulta: -1 });
+  if(consultas.length === 0){
+    res.status(404).json({ message: "Nenhum registro de consulta encontrado."})
+  }
+  res.status(200).json(consultas);
+}
+
+export const putSaude = async (req: Request, res: Response) =>{
+  const usuarioTipo = req.user?.tipo;
+  if (usuarioTipo !== "enfermeiro") {
+    return res.status(401).json({ message: "Somente enfermeiros podem registrar acompanhamento de saúde" });
+  }
+  const consulta = await Saude.findByIdAndUpdate(
+    req.params.id, 
+    req.body,
+    { new:true }
+    );
+
+    if(!consulta){
+      return res.status(404).json({ message:"Nenhum registro de consulta encontrado. "})
+    }
+    res.status(200).json({message: "Atualizado com sucesso!", consulta});
+}
+
+//essa função irá excluir apenas a consulta em específico.
+export const deleteSaude = async (req: Request, res: Response) => {
+  const usuarioTipo = req.user?.tipo;
+  if (usuarioTipo !== "enfermeiro") {
+    return res.status(401).json({ message: "Somente enfermeiros podem registrar acompanhamento de saúde" });
+  }
+
+  const consulta = await Saude.findByIdAndDelete(req.params.id);
+  if(!consulta){
+    return res.status(404).json({message:"Nenhum registro de consulta encontrado."})
+  }
+  res.status(200).json({message:"Deletado com sucesso!", consulta});
 }
 
--- src/routes/saudeRoutes.ts
+++ src/routes/saudeRoutes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { authenticate } from "../middlewares/autenticacaoMiddleware";
-import { createSaude, getSaude} from "../controllers/saudeController";
+import { createSaude, getSaude, getSaudeById, putSaude, deleteSaude } from "../controllers/saudeController";
 
 const routerSaude = Router();
 
-routerSaude.post('/cadastrarSaude', authenticate, createSaude);
+routerSaude.post('/cadastrarConsulta', authenticate, createSaude);
 routerSaude.get('/saudeIdoso', authenticate, getSaude);
+routerSaude.get('/saudeIdoso/:idosoId', authenticate, getSaudeById);
+routerSaude.put('/saudeIdoso/:id', authenticate, putSaude); //id da consulta
+routerSaude.delete('/saudeIdoso/:id', authenticate, deleteSaude); //id da consulta
 
 export default routerSaude;
