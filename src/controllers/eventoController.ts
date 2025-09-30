e06d28c0cd17e654400f0103539fee268f07bca7
Ana Beatriz <anabeatrizqueiroza@gmail.com>
2025-09-30 19:51:31 -0300

    Ajustando o import do model em acompanhamentoController e eventoController

--- src/controllers/eventoController.ts
+++ src/controllers/eventoController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import Evento from "../models/evento";
+import Evento from "../models/Evento";
 import { any, ZodError } from "zod";
 import { createEventoSchema } from "../validations/eventoValidations";
 
