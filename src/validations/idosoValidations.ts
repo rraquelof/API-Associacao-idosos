8842086a5674ce62669a6898c98c0ae9aafedb13
Raquel Oliveira Ferreira <rraquelof@gmail.com>
2025-11-10 20:05:43 -0300

    mudando para dataNascimento

--- src/validations/idosoValidations.ts
+++ src/validations/idosoValidations.ts
@@ -9,7 +9,7 @@ export const createIdosoSchema = z.object({
     cpf: z.string().regex(cpfRegex, "Formato CPF inválido. Formato esperado: xxx.xxx.xxx-xx"),
     rg: z.string().regex(rgRegex, "Formato RG inválido. Formato esperado: x.xxx.xxx"),
     sus: z.string().regex(susRegex, "Formato SUS inválido. Cartão SUS deve conter 15 dígitos"),
-    data_nascimento: z.string().refine((date) => !isNaN(Date.parse(date)), { message: "Data de nascimento inválida" }),
+    dataNascimento: z.string().refine((date) => !isNaN(Date.parse(date)), { message: "Data de nascimento inválida" }),
     sexo: z.enum(['masculino', 'feminino'], "Sexo deve ser 'masculino' ou 'feminino'"),
     nacionalidade: z.string().min(3, "Nacionalidade deve ter pelo menos 3 caracteres"),
     naturalidade: z.string().min(3, "Naturalidade deve ter pelo menos 3 caracteres"),
