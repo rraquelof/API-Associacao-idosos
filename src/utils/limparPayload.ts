44382cd01fa858377dd69c2f2326ba16759549fb
Darkiane <darkianelima13@gmail.com>
2025-12-30 22:02:46 -0300

    apagando função

--- src/utils/limparPayload.ts
+++
@@ -1,8 +0,0 @@
-export function limparPayload(obj: any) {
-  return Object.fromEntries(
-    Object.entries(obj).map(([key, value]) => [
-      key,
-      value === "" ? undefined : value,
-    ])
-  );
-}
